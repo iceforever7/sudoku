c826d4270b8aee56fea2b0bed15eeaccf654cf34
iceforever7 <2505565012@qq.com>
2023-10-07 12:40:28 +0800

    Delete js directory

--- js/value.js
+++
@@ -1,8 +0,0 @@
-function getId(event) {
-  const input = event.target;
-  const inputId = input.id;
-}
-function addNumber(number) {
-  var input = document.getElementById("inputId");
-  input.value = parseInt(input.value) + number;
-}
